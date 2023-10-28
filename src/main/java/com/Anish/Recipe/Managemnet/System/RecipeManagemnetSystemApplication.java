98df7e1fd23eb7416f7cb11050ce58dd96525534
Anish Kumar Sharma <anish.kumar20071998@gmail.com>
2023-10-28 10:43:50 +0530

    "First Commit with Application.properties setup "

---
+++ src/main/java/com/Anish/Recipe/Managemnet/System/RecipeManagemnetSystemApplication.java
@@ -0,0 +1,14 @@
+package com.Anish.Recipe.Managemnet.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeManagemnetSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeManagemnetSystemApplication.class, args);
+		System.out.println("Anish is Creating a Project");
+	}
+
+}
---
+++ src/test/java/com/Anish/Recipe/Managemnet/System/RecipeManagemnetSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Anish.Recipe.Managemnet.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeManagemnetSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
